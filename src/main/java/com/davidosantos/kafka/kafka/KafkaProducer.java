3bc0a69264e7b0d48e8857eba35b8038649cfc28
davidosantos <davidosantos@hotmail.com>
2022-11-24 14:27:42 -0300

    .

--- src/main/java/com/davidosantos/kafka/kafka/KafkaProducer.java
+++ src/main/java/com/davidosantos/kafka/kafka/KafkaProducer.java
@@ -13,22 +13,22 @@ import net.andreinc.mockneat.MockNeat;
 @Log4j2
 public class KafkaProducer {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, User> kafkaTemplate;
 
     private final MockNeat mock = MockNeat.threadLocal();
 
     @Scheduled(fixedRate = 500, initialDelay = 2000)
     public void generate() {
 
-        Users user = mock.reflect(Users.class)
-                .field("id", mock.longs().range(1l, 30_000l).get())
-                .field("name", mock.names())
-                .field("email", mock.emails())
-                .val();
-
+        User user = User.newBuilder()
+        .setId(mock.longs().range(1l, 30_000l).get())
+        .setName(mock.names().get())
+        .setEmail(mock.emails().get())
+        .build();
+        
         log.info("Sending message to Kafka: " + user.toString());
 
-        kafkaTemplate.send("harryPotterBookShelf", user.getEmail(), user.toString());
+        kafkaTemplate.send("harryPotterBookShelf-avro", user.getEmail().toString(), user);
 
     }
 }
